c1fc805fb8ced812b494855c60e5c11d728af74e
xiaoke.luo <xiaoke.luo@tcl.com>
2021-02-04 18:14:58 +0800

    add key

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class Board extends React.Component {
 	renderSquare(i) {
 		return (
 			<Square
+				key={i}
 				value={this.props.squares[i]}
 				onClick={() => this.props.onClick(i)}
 			/>
@@ -27,7 +28,7 @@ class Board extends React.Component {
 				return this.renderSquare(c);
 			});
 			return (
-				<div className="board-row"  >
+				<div className="board-row" key={rIndex} >
 					{column}
 				</div>
 			);
